da1c722533451caab6759b404465e28c1673f1c5
tshiva12 <shivaprasad233307@msitprogram.net>
2018-10-31 17:28:56 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/m3/3.1 Directed Cycle/Solution.java
+++ ASSIGNMENTS/m3/3.1 Directed Cycle/Solution.java
@@ -3,34 +3,37 @@ import java.util.Scanner;
  * main class.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		// default constructor is not used.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		Digraph digraph = new Digraph(vertices);
-		while (edges > 0) {
-			String[] tokens = scan.nextLine().split(" ");
-			int a = Integer.parseInt(tokens[0]);
-			int b = Integer.parseInt(tokens[1]);
-			digraph.addEdge(a, b);
-			edges--;
-		}
-		DirectedCycle dcycle = new DirectedCycle(digraph);
-		if (dcycle.hasCycle()) {
-			System.out.println("Cycle exists.");
-		} else {
-			System.out.println("Cycle doesn't exists.");
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // default constructor is not used.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        Digraph digraph = new Digraph(vertices);
+        while (edges > 0) {
+            String[] tokens = scan.nextLine().split(" ");
+            int a = Integer.parseInt(tokens[0]);
+            int b = Integer.parseInt(tokens[1]);
+            digraph.addEdge(a, b);
+            edges--;
+        }
+        DirectedCycle dcycle = new DirectedCycle(digraph);
+        if (dcycle.hasCycle()) {
+            System.out.println("Cycle exists.");
+        } else {
+            System.out.println("Cycle doesn't exists.");
+        }
+    }
 }
+
+
+
