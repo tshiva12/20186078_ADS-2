938e2eb51e1d9214fb2c07d9ce3ef0bea87eebe7
tshiva12 <shivaprasad233307@msitprogram.net>
2018-11-01 23:02:42 +0530

    "ads2_m4_a1_1: 7/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/m4/Assignment1/Code Camp - WordNet/Solution.java
+++ ASSIGNMENTS/m4/Assignment1/Code Camp - WordNet/Solution.java
@@ -2,31 +2,33 @@
  * class Solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		// default constructor is not used.
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // default constructor is not used.
+    }
 
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		String synsets = StdIn.readString();
-		String hypernyms = StdIn.readString();
-		String strtype1 = StdIn.readString();
-		if (strtype1.equals("Graph")) {
-			WordNet wordnet = new WordNet(synsets, hypernyms);
-		}
-		if (strtype1.equals("Queries")) {
-			String[] str1 = StdIn.readString().split(" ");
-			if (str1[0].equals("null")) {
-				System.out.println("IllegalArgumentException");
-			}
-		}
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String synsets = StdIn.readString();
+        String hypernyms = StdIn.readString();
+        String strtype1 = StdIn.readString();
+        if (strtype1.equals("Graph")) {
+            WordNet wordnet = new WordNet(synsets, hypernyms);
+        }
+        if (strtype1.equals("Queries")) {
+            String[] str1 = StdIn.readString().split(" ");
+            if (str1[0].equals("null")) {
+                System.out.println("IllegalArgumentException");
+            }
+        }
 
-	}
+    }
 }
+
+
