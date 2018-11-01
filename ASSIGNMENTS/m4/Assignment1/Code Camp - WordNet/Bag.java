1a2c5238b8a5329931f273f2dc94a6d3df5a7c8a
tshiva12 <shivaprasad233307@msitprogram.net>
2018-11-01 22:58:15 +0530

    "ads2_m4_a1_1: 7/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/m4/Assignment1/Code Camp - WordNet/Bag.java
+++ ASSIGNMENTS/m4/Assignment1/Code Camp - WordNet/Bag.java
@@ -1,132 +1,115 @@
-/******************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *  Dependencies: StdIn.java StdOut.java
- *
- *  A generic bag or multiset, implemented using a singly linked list.
- *
- *  % more tobe.txt 
- *  to be or not to - be - - that - - - is
- *
- *  % java Bag < tobe.txt
- *  size of bag = 14
- *  is
- *  -
- *  -
- *  -
- *  that
- *  -
- *  -
- *  be
- *  -
- *  to
- *  not
- *  or
- *  be
- *  to
- *
- ******************************************************************************/
-
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
- *  The {@code Bag} class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
- *  items in arbitrary order.
- *  <p>
- *  This implementation uses a singly linked list with a static nested class Node.
- *  See {@link LinkedBag} for the version from the
- *  textbook that uses a non-static nested class.
- *  See {@link ResizingArrayBag} for a version that uses a resizing array.
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for bag.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
- *  @param <Item> the generic type of an item in this bag
+ * @param      <Item>  The item
  */
 public class Bag<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of bag
-    private int n;               // number of elements in bag
-
-    // helper linked list class
-    private static class Node<Item> {
-        private Item item;
-        private Node<Item> next;
-    }
-
     /**
-     * Initializes an empty bag.
+     * Number of elements in bag.
      */
+    private int n;
+    /**
+     * Node variable.
+     */
+    private Node first;
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * variable to store the item value.
+         */
+        private Item item;
+        /**
+         * Node variable.
+         */
+        private Node next;
+    }
+    /**
+    * Create an empty stack.
+    */
     public Bag() {
         first = null;
         n = 0;
     }
 
     /**
-     * Returns true if this bag is empty.
+     * Is the BAG empty?
      *
-     * @return {@code true} if this bag is empty;
-     *         {@code false} otherwise
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return first == null;
     }
-
     /**
-     * Returns the number of items in this bag.
+     * Return the number of items in the bag.
      *
-     * @return the number of items in this bag
+     * @return     size of bag.
      */
     public int size() {
         return n;
     }
-
     /**
-     * Adds the item to this bag.
+     * Add the item to the bag.
+     * Time complexity is O(1).
      *
-     * @param  item the item to add to this bag
+     * @param      item  The item
      */
-    public void add(Item item) {
-        Node<Item> oldfirst = first;
-        first = new Node<Item>();
+    public void add(final Item item) {
+        Node oldfirst = first;
+        first = new Node();
         first.item = item;
         first.next = oldfirst;
         n++;
     }
-
-
     /**
-     * Returns an iterator that iterates over the items in this bag in arbitrary order.
+     * Return an iterator that iterates over the items in the bag.
      *
-     * @return an iterator that iterates over the items in this bag in arbitrary order
+     * @return     iterator.
      */
     public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
+        return new ListIterator();
     }
-
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
+    /**
+     * Class for list iterator.
+     */
+    private class ListIterator implements Iterator<Item> {
+        /**
+         * Node variable.
+         */
+        private Node current = first;
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            return current != null;
         }
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**
+         * remove operation.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * next method returns the item.
+         *
+         * @return     item in the bag.
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
 }
+
+
+
+
