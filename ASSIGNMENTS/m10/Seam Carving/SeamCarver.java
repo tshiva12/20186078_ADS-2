c245b1962319603f2999481b03c4221d686fc6e6
tshiva12 <shivaprasad233307@msitprogram.net>
2018-11-08 15:18:27 +0530

    "ads2_m10_a1_1: 1/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/m10/Seam Carving/SeamCarver.java
+++ ASSIGNMENTS/m10/Seam Carving/SeamCarver.java
@@ -2,10 +2,11 @@
 public class SeamCarver {
 	private Picture picture;
 	// create a seam carver object based on the given picture
-	public SeamCarver(Picture picture) {
+	public SeamCarver(Picture picture1) {
 		if (picture == null) {
 			throw new IllegalArgumentException("picture is null");
 		}
+		this.picture = picture1;
 	}
 	// current picture
 	public Picture picture() {
@@ -23,9 +24,6 @@ public class SeamCarver {
 
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
-		if (x == 0 || y == 0 || picture.width() - 1 == x || picture.height() - 1 == y) {
-			return 1000;
-		}
 		return 0;
 	}
 
