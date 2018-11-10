c238d03325ae6ddf7eef802c0998930dd04233c6
tshiva12 <shivaprasad233307@msitprogram.net>
2018-11-10 12:50:58 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ASSIGNMENTS/m12mid2/ADS-2_Exam-2/Solution.java
+++ ASSIGNMENTS/m12mid2/ADS-2_Exam-2/Solution.java
@@ -78,13 +78,14 @@ public final class Solution {
             String path = "";
             if (dij1.hasPathTo(via) && dij2.hasPathTo(dest1)) {
                 for (int edge1 : dij1.pathTo(via)) {
-                    path += edge1;
+                    path += edge1 + " ";
                 }
                 for (int edge2 : dij2.pathTo(dest1)) {
-                    path += edge2;    
+                    path += edge2 + " ";    
                 }
                 path += token2[2];
-                System.out.println(dij1.distTo(via) + dij2.distTo(dest1));
+                System.out.println(dij1.distTo(via)
+                 + dij2.distTo(dest1));
                 System.out.println(path);
             } else {
                 System.out.println("No Path Found.");
