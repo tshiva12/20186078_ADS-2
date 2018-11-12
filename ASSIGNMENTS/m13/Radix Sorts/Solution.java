16ccf4c37ae601a1d3dbc12f089a95a875fd27e8
tshiva12 <shivaprasad233307@msitprogram.net>
2018-11-12 13:49:00 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/m13/Radix Sorts/Solution.java
+++ ASSIGNMENTS/m13/Radix Sorts/Solution.java
@@ -20,18 +20,18 @@ class LSD {
      */
     public static void sort(String[] a, int w) {
         int n = a.length;
-        int R = 256;   // extend ASCII alphabet size
+        int r = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
-        for (int d = w-1; d >= 0; d--) {
+        for (int d = w - 1; d >= 0; d--) {
             // sort by key-indexed counting on dth character
             // compute frequency counts
-            int[] count = new int[R+1];
+            int[] count = new int[r + 1];
             for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
             }
             // compute cumulates
-            for (int r = 0; r < R; r++) {
-                count[r+1] += count[r];
+            for (int i = 0; i < r; i++) {
+                count[i + 1] += count[i];
             }
             // move data
             for (int i = 0; i < n; i++) {
