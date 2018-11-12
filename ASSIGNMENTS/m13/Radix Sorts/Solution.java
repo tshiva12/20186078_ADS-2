8926075fbcea032933b2cee795fb84d21a758036
tshiva12 <shivaprasad233307@msitprogram.net>
2018-11-12 13:46:04 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/m13/Radix Sorts/Solution.java
+++ ASSIGNMENTS/m13/Radix Sorts/Solution.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for lsd.
+ */
 class LSD {
+	/**
+	 * Integer var
+	 */
     private static final int BITS_PER_BYTE = 8;
-
-    // do not instantiate
+    /**
+     * Constructs the object.
+     */
     protected LSD() { }
-
-   /**  
+   	/**  
      * Rearranges the array of W-character strings in ascending order.
      *
      * @param a the array to be sorted
@@ -16,34 +22,44 @@ class LSD {
         int n = a.length;
         int R = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
-
         for (int d = w-1; d >= 0; d--) {
             // sort by key-indexed counting on dth character
-
             // compute frequency counts
             int[] count = new int[R+1];
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
-
+            }
             // compute cumulates
-            for (int r = 0; r < R; r++)
+            for (int r = 0; r < R; r++) {
                 count[r+1] += count[r];
-
+            }
             // move data
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 aux[count[a[i].charAt(d)]++] = a[i];
-
+            }
             // copy back
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 a[i] = aux[i];
+            }
         }
     }
 }
+/**
+ * class Solution.
+ */
 public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
 		// default constructor is not used.
 	}
-	public static void main(String[] args) {
+	/**
+	 * main function.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = Integer.parseInt(scan.nextLine());
 		String[] str1 = new String[n];
