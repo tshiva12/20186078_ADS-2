bd51d9259b806f45a23af3567638c8dd6f8effda
tshiva12 <shivaprasad233307@msitprogram.net>
2018-11-12 13:52:35 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ASSIGNMENTS/m13/Radix Sorts/Solution.java
+++ ASSIGNMENTS/m13/Radix Sorts/Solution.java
@@ -4,23 +4,23 @@ import java.util.Arrays;
  * Class for lsd.
  */
 class LSD {
-	/**
-	 * Integer var
-	 */
+    /**
+     * Integer variable.
+     */
     private static final int BITS_PER_BYTE = 8;
     /**
      * Constructs the object.
      */
     protected LSD() { }
-   	/**  
+    /**
      * Rearranges the array of W-character strings in ascending order.
      *
      * @param a the array to be sorted
      * @param w the number of characters per string
      */
-    public static void sort(String[] a, int w) {
+    public static void sort(final String[] a, final int w) {
         int n = a.length;
-        int r = 256;   // extend ASCII alphabet size
+        final int r = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
         for (int d = w - 1; d >= 0; d--) {
             // sort by key-indexed counting on dth character
@@ -48,26 +48,29 @@ class LSD {
  * class Solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		// default constructor is not used.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = Integer.parseInt(scan.nextLine());
-		String[] str1 = new String[n];
-		for (int i = 0; i < str1.length; i++) {
-			str1[i] = scan.nextLine();
-		}
-		LSD lsd = new LSD();
-		lsd.sort(str1, str1[0].length());
-		System.out.println(Arrays.toString(str1));
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // default constructor is not used.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        String[] str1 = new String[n];
+        for (int i = 0; i < str1.length; i++) {
+            str1[i] = scan.nextLine();
+        }
+        LSD lsd = new LSD();
+        lsd.sort(str1, str1[0].length());
+        System.out.println(Arrays.toString(str1));
+    }
 }
+
+
+
