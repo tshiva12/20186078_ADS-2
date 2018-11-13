35eddc5ba52725399f7f7ee223faa19288dc533e
tshiva12 <shivaprasad233307@msitprogram.net>
2018-11-13 16:43:47 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/m14/Assignment-1/Solution.java
+++ ASSIGNMENTS/m14/Assignment-1/Solution.java
@@ -1,26 +1,43 @@
 import java.util.Scanner;
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		String[] words = loadWords();
-		TST<Integer> tst = new TST<Integer>();
-		Scanner scan = new Scanner(System.in);
-		String prefix = scan.nextLine();
-		int j = 0;
-		for (String word : words) {
-			SuffixArray sa = new SuffixArray(word);
-			for (int i = 0; i < word.length(); i++) {
-				tst.put(sa.select(i), j++);
-			}
-		}
-		for (String str : tst.keysWithPrefix(prefix)) {
-			System.out.println(str);
-		}
-	}
-
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // default constructor is not used.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        String[] words = loadWords();
+        TST<Integer> tst = new TST<Integer>();
+        Scanner scan = new Scanner(System.in);
+        String prefix = scan.nextLine();
+        int j = 0;
+        for (String word : words) {
+            SuffixArray sa = new SuffixArray(word);
+            for (int i = 0; i < word.length(); i++) {
+                tst.put(sa.select(i), j++);
+            }
+        }
+        for (String str : tst.keysWithPrefix(prefix)) {
+            System.out.println(str);
+        }
+    }
+    /**
+     * Loads words.
+     *
+     * @return     words
+     */
+    public static String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
