9a9460c8fc5cbf7c1260bbc04f380597e036b9a3
tshiva12 <shivaprasad233307@msitprogram.net>
2018-11-16 11:06:08 +0530

    "ads2_m16_a1_1: 22/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ASSIGNMENTS/m16/boggle/BoggleSolver.java
+++ ASSIGNMENTS/m16/boggle/BoggleSolver.java
@@ -1,9 +1,14 @@
+import java.util.Set;
+import java.util.TreeSet;
 public class BoggleSolver {
 	private TrieST<Integer> tries;
+	private Set<String> validwords;
+	private boolean[][] check;
 	// Initializes the data structure using the given array of strings as the dictionary.
 	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
 	public BoggleSolver(String[] dictionary) {
 		tries = new TrieST<Integer>();
+		validwords = new TreeSet<String>();
 		int[] points = {0, 0, 0, 1, 1, 2, 3, 5, 11};
 		for (String eachword : dictionary) {
 			if (eachword.length() >= 8) {
@@ -21,9 +26,46 @@ public class BoggleSolver {
 		if (board == null) {
 			throw new IllegalArgumentException("board is null");
 		}
-		return new Bag<String>();
+		check = new boolean[board.rows()][board.cols()];
+		for (int i = 0; i < board.rows(); i++) {
+			for (int j = 0; j < board.cols(); j++) {
+				String str = addChar("", board.getLetter(i, j));
+				dfs(board, check, i, j, str);				
+			}
+		}
+		return validwords;
 	}
-
+	private String addChar(String str, char c) {
+		if (c == 'Q') {
+			return str + "QU";
+		} else {
+			return str + c;
+		}
+	}
+	private boolean isValid(String word) {
+		if (word.length() < 3) {
+			return false;
+		}
+		return tries.contains(word);
+	}
+	public void dfs(BoggleBoard board, boolean[][] check, int rows, int cols, String word) {
+		if (isValid(word)) {
+			validwords.add(word);
+		}
+		check[rows][cols] = true;
+		for (int i = rows - 1; i <= rows + 1; i++) {
+			for (int j = cols - 1; j <= cols + 1; j++) {
+				if (isValidRowColumn(i, j, board) && !check[i][j]) {
+					String str1 = addChar(word, board.getLetter(i, j));
+					dfs(board, check, i, j, str1);
+				}
+			}
+		}
+		check[rows][cols] = false;
+	}
+	private boolean isValidRowColumn(int row, int col, BoggleBoard board) {
+		return (row >= 0 && col >= 0 && row < board.rows() && col < board.cols());
+	}
 	// Returns the score of the given word if it is in the dictionary, zero otherwise.
 	// (You can assume the word contains only the uppercase letters A through Z.)
 	public int scoreOf(final String word) {
